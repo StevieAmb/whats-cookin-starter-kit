e846dfd49bd1e438ceacaddee1b1e8291121b612
Lindsay Schnell <lschnell8@gmail.com>
2021-12-07 13:50:26 -0700

    Build out skeleton

---
+++ test/Ingredient-test.js
@@ -0,0 +1,56 @@
+import { expect } from 'chai';
+import Ingredient from '../src/classes/Ingredient';
+
+describe('Recipe', () => {
+
+  beforeEach(() => {   
+
+    it('Should be a function', () => {
+      expect(Ingredient).to.be.a('function');
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+    
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  });
+});
