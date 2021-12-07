52cb97af8e4e0ade9ac79a6f7fb4b9699f4e71ee
Lindsay Schnell <lschnell8@gmail.com>
2021-12-07 13:51:08 -0700

    Build out skeleton

---
+++ test/Recipe-test.js
@@ -0,0 +1,57 @@
+import { expect } from 'chai';
+import { beforeEach } from 'mocha';
+import Recipe from '../src/classes/Recipe';
+
+describe('Recipe', () => {
+
+  beforeEach(() => {
+
+    it('Should be a function', () => {
+      expect(Recipe).to.be.a('function');
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+    
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  });
+});
