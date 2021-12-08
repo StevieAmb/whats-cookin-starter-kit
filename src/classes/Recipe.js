ff6c3fb702431b4c2b59603a6ea6c7e601becfa9
Stevie Ambroise <s.ambroise@protonmail.com>
2021-12-07 18:30:13 -0700

    Finish adding properties and a function to recipe class

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,3 +1,5 @@
+import Ingredient from "./Ingredient";
+
 class Recipe {
   constructor(id, image, ingredients, instructions, name, tags) {
     this.id = id;
@@ -7,6 +9,31 @@ class Recipe {
     this.name = name;
     this.tags = tags;
   }
+
+  determineIngredient(recipeId) {
+    const ingredients = new Ingredients();
+    ingredients.ingredients.filter(ingredient => {
+      if(recipeId === ingredient.id) {
+        return ingredient;
+      }
+    }
+  }
+
+  determineCost(recipeId) {
+    const ingredients = new Ingredients();
+    ingredients.ingredients.filter(ingredient => {
+      if(recipeId === ingredient.id) {
+
+        const totalCents += ingredients.estimatedCostInCents;
+
+      }
+    })
+
+  }
+  //need to get access to the ingredients of a specific recipe
+  //the costs and the units will be multiplied together
+  //the cost is in the ingredients file 
+  //the units are in recipe property 
 };
 
 export default Recipe;
