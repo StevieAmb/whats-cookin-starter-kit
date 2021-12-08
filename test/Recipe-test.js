de99ca09ab2ae4d4372d602c2b920d055b7edbd8
Stevie Ambroise <s.ambroise@protonmail.com>
2021-12-07 17:36:50 -0700

    Commence recipe test file

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -3,38 +3,39 @@ import { beforeEach } from 'mocha';
 import Recipe from '../src/classes/Recipe';
 
 describe('Recipe', () => {
-
+const recipe;
   beforeEach(() => {
-
+    recipe = new Recipe(1, "src", "ingredients", ['food', 'food', 'food'], "Spaghetti", ['pasta', 'brekkie']);
+    
     it('Should be a function', () => {
       expect(Recipe).to.be.a('function');
     });
   
-    it('Should ', () => {
+    it('Should be an instance of Recipe', () => {
+      expect(recipe).to.be.an.instanceOf(Recipe);
+    });
+  
+    it('Should have an id', () => {
+      expect(recipe.id).to.equal(1);
+    });
+  
+    it('Should have an image', () => {
+      expect(recipe.image).to.equal('src');
+    });
+  
+    it('Should have ingredients', () => {
+      expect(recipe.ingredients).to.equal('ingredients');
+    });
+  
+    it('Should have instructions', () => {
       expect().to.be.a();
     });
   
-    it('Should ', () => {
+    it('Should have a name', () => {
       expect().to.be.a();
     });
   
-    it('Should ', () => {
-      expect().to.be.a();
-    });
-  
-    it('Should ', () => {
-      expect().to.be.a();
-    });
-  
-    it('Should ', () => {
-      expect().to.be.a();
-    });
-  
-    it('Should ', () => {
-      expect().to.be.a();
-    });
-  
-    it('Should ', () => {
+    it('Should have tags', () => {
       expect().to.be.a();
     });
   
