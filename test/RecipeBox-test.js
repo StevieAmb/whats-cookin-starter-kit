ae08d29d9eb3f322a084fc54dd4d4c8fa8c04bfb
Stevie Ambroise <s.ambroise@protonmail.com>
2021-12-07 18:32:22 -0700

    Add instance of recipe box to test file

--- test/RecipeBox-test.js
+++ test/RecipeBox-test.js
@@ -2,15 +2,17 @@ import { expect } from 'chai';
 import RecipeBox from '../src/classes/RecipeBox';
 
 describe('Recipe', () => {
+const recipeBox;
 
   beforeEach(() => {
+    recipeBox = new RecipeBox('recipe')
 
-    it('Should be a function', () => {
+    it.only('Should be a function', () => {
       expect(RecipeBox).to.be.a('function');
     });
   
-    it('Should ', () => {
-      expect().to.be.a();
+    it('Should be an instance of RecipeBox ', () => {
+      expect(recipeBox).to.be.an.instanceOf(RecipeBox);
     });
   
     it('Should ', () => {
