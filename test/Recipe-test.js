5075b70ff14a34eb5f90410b0c7b486fe8813378
Stevie Ambroise <s.ambroise@protonmail.com>
2021-12-07 19:43:22 -0700

    Add instance of ingredient class to test file

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -4,11 +4,13 @@ import Recipe from '../src/classes/Recipe';
 
 describe('Recipe', () => {
 const recipe;
+const ingredientsData;
   beforeEach(() => {
     recipe1 = new Recipe(1, "src1", [{"ingred1"}, {"ingred2"}, {"ingred3"}], [{"instruct1"}, {"instruct2"}, {"instruct3"}], "Spaghetti", ["pasta", "main"])
     recipe2 = new Recipe(2, "src2", [{"ingred1"}, {"ingred2"}, {"ingred3"}], [{"instruct1"}, {"instruct2"}, {"instruct3"}], "Grilled Cheese", ["sandwich", "main"])
     recipe3 = new Recipe(3, "src3", [{"ingred1"}, {"ingred2"}, {"ingred3"}], [{"instruct1"}, {"instruct2"}, {"instruct3"}], "CheeseCake", ["dessert", "sweet"])
-    
+    ingredientsData1 = new Ingredients(1, "baking soda", 475);
+
     it('Should be a function', () => {
       expect(Recipe).to.be.a('function');
     });
@@ -40,6 +42,8 @@ const recipe;
     it('Should have tags', () => {
       expect(recipe.tags).to.equal(['pasta', 'brekkie']);
     });
+    recipe.determineCost(1);
+    console.log(result);
   
     it('Should ', () => {
       expect().to.be.a();
