afabc935acb6907ccb586121f26389cb2ef8d1d8
Stevie Ambroise <s.ambroise@protonmail.com>
2021-12-07 19:45:25 -0700

    Creat instance of recipebox in test file

--- test/RecipeBox-test.js
+++ test/RecipeBox-test.js
@@ -5,7 +5,10 @@ describe('Recipe', () => {
 const recipeBox;
 
   beforeEach(() => {
-    recipeBox = new RecipeBox('recipe')
+    const recipe1 = new Recipe(1, "src1", [{"ingred1"}, {"ingred2"}, {"ingred3"}], [{"instruct1"}, {"instruct2"}, {"instruct3"}], "Spaghetti", ["pasta", "main"])
+    const recipe2 = new Recipe(2, "src2", [{"ingred1"}, {"ingred2"}, {"ingred3"}], [{"instruct1"}, {"instruct2"}, {"instruct3"}], "Grilled Cheese", ["sandwich", "main"])
+    const recipe3 = new Recipe(3, "src3", [{"ingred1"}, {"ingred2"}, {"ingred3"}], [{"instruct1"}, {"instruct2"}, {"instruct3"}], "CheeseCake", ["dessert", "sweet"])
+    const recipeBox = new RecipeBox(recipe1, recipe2, recipe3)
 
     it.only('Should be a function', () => {
       expect(RecipeBox).to.be.a('function');
@@ -15,7 +18,7 @@ const recipeBox;
       expect(recipeBox).to.be.an.instanceOf(RecipeBox);
     });
   
-    it('Should ', () => {
+    it('Should ', () =>g
       expect().to.be.a();
     });
   
