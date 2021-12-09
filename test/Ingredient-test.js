85557452a703f4c60213ec29d3bfb5944d170724
Lindsay Schnell <lschnell8@gmail.com>
2021-12-08 19:33:17 -0700

    Add file path to data file and sample data

---
+++ test/Ingredient-test.js
@@ -0,0 +1,66 @@
+import { expect } from 'chai';
+import Ingredient from '../src/classes/Ingredient';
+import ingredientsData from '../src/data/ingredients'
+
+
+describe('Ingredient', () => {
+let ingredient1, ingredient2, ingredient3, ingredientsRepo;
+
+  beforeEach(() => {   
+    ingredient1 = new Ingredient(data.ingredientsData[0]);
+
+    ingredient2 = new Ingredient(data.ingredientsData[1]);
+
+    ingredient3 = new Ingredient(data.ingredientsData[2]);
+
+    ingredientsRepo = [ingredient1, ingredient2, ingredient3];
+
+    it('Should be a function', () => {
+      expect(Ingredient).to.be.a('function');
+    });
+  
+    it('Should be an instance of Ingredient ', () => {
+      expect().to.be.an.instanceOf(Ingredient);
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+    
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  });
+});
