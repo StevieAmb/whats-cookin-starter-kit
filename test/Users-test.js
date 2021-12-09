0a866d54ba184a83efa86b5dafb58c2cdbf5af67
Lindsay Schnell <lschnell8@gmail.com>
2021-12-08 19:34:13 -0700

    Add file path to data file and sample data

---
+++ test/Users-test.js
@@ -0,0 +1,59 @@
+import { expect } from 'chai';
+import Users from '../src/classes/Users';
+import usersData from '../src/data/users'
+
+describe('Recipe', () => {
+  let users;
+  beforeEach(() => {
+
+    users = new Users(usersData)
+
+    it('Should be a function', () => {
+      expect(Users).to.be.a('function');
+    });
+  
+    it('Should be an instance of Users', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  
+    it('Should ', () => {
+      expect().to.be.a();
+    });
+  });
+});
