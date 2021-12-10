ca5411df1493b21adcb51b3baff6cc4a384abc6e
Lindsay Schnell <lschnell8@gmail.com>
2021-12-10 01:31:04 -0700

    Remove unnecessary code, rewrite existing tests and add missing tests

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -1,69 +1,77 @@
-
 import { expect } from 'chai';
-import RecipeBox from '../src/classes/RecipeBox';
-import recipeData from '../src/data/recipes';
+// import { recipeData } from '../src/data/recipes';
 import Recipe from '../src/classes/Recipe';
 
 describe('Recipe', () => {
-  let recipeBox;
-  let recipe1;
-  let recipe2;
-  let recipe3;
-  let recipeArray;
-
+  let myRecipe;
+  
   beforeEach(() => {
-    recipeArray = recipeData;
-    recipe1 = new Recipe(recipeArray.recipeData[0].id, recipeArray.recipeData[0].image, recipeArray.recipeData[0].ingredients, recipeArray.recipeData[0].instructions, recipeArray.recipeData[0].name, recipeArray.recipeData[0].tags)
-    recipe2 = new Recipe(2, "src2", [{ingred1: "ingred1"}, {ingred2: "ingred2"}, {ingred3: "ingred3"}], [{instruct1: "instruct1"}, {instruct2: "instruct2"}, {instruct3: "instruct3"}], "Grilled Cheese", ["sandwich", "main"])
-    recipe3 = new Recipe(3, "src3", [{ingred1: "ingred1"}, {ingred2: "ingred2"}, {ingred3: "ingred3"}], [{instruct1: "instruct1"}, {instruct2: "instruct2"}, {instruct3: "instruct3"}], "CheeseCake", ["dessert", "sweet"])
-    recipeBox = new RecipeBox(recipe1, recipe2, recipe3)
-    console.log(recipeArray.recipeData[0].id);
+    myRecipe = new Recipe(recipeData[0]);
   });
 
     it('Should be a function', () => {
       expect(Recipe).to.be.a('function');
     });
-  
+    
     it('Should be an instance of Recipe', () => {
-      expect(recipe1).to.be.an.instanceOf(Recipe);
+      expect(myRecipe).to.be.an.instanceOf(Recipe);
     });
-  
+    
     it('Should have an id', () => {
-      expect(recipe1.id).to.equal(recipeArray.recipeData[0].id);
+      expect(myRecipe.id).to.equal(recipeData[0].id);
     });
-  
+    
     it('Should have an image', () => {
-      expect(recipe1.image).to.equal(recipeArray.recipeData[0].image);
+      expect(myRecipe.image).to.equal(recipeData[0].image);
     });
-  
-    it('Should have ingredients', () => {
-      expect(recipe1.ingredients).to.deep.equal(recipeArray.recipeData[0].ingredients);
+
+    it('Should have a list of ingredients', () => {
+      expect(myRecipe.ingredients).to.equal(recipeData[0].ingredients);
     });
-  
-    it('Should have instructions', () => {
-      expect(recipe1.instructions).to.deep.equal(recipeArray.recipeData[0].instructions);
+
+    it('Should have a list of instructions', () => {
+      expect(myRecipe.instructions).to.equal(recipeData[0].instructions);
     });
-  
+
     it('Should have a name', () => {
-      expect(recipe1.name).to.equal(recipeArray.recipeData[0].name);
+      expect(myRecipe.name).to.equal(recipeData[0].name);
     });
   
     it('Should have tags', () => {
-      expect(recipe1.tags).to.deep.equal(recipeArray.recipeData[0].tags);
+      expect(myRecipe.tags).to.deep.equal(recipeData[0].tags);
     });
-  
-    it('Should return a list of ingredients needed for a Recipe ', () => {
-      expect(recipe.determineIngredients(595736)).to.be.a();
-    });
-  
-    // it('Should ', () => {
-    //   expect().to.be.a();
-    // });
-  
-    // it('Should ', () => {
-    //   expect().to.be.a();
-    // });
     
-    // it('Should ', () => {
-    //   expect().to.be.a();
+    it('Should return the names of the ingredients in a recipe', () => {
+      console.log(myRecipe.findRecipeIngredientInfo());
+      const expected = [
+        'wheat flour',
+        'bicarbonate of soda',
+        'eggs',
+        'sucrose',
+        'instant vanilla pudding',
+        'brown sugar',
+        'salt',
+        'fine sea salt',
+        'semi sweet chips',
+        'unsalted butter',
+        'vanilla'
+      ]
+
+      const actual = myRecipe.findRecipeIngredientInfo();
+
+      expect(actual).to.deep.equal(expected);
     });
+    
+
+    it('Should have instructions', () => {
+      expect(myRecipe.getRecipeInstructions()).to.deep.equal(recipeData[0].instructions);
+    });
+  
+    it('Should return recipe instructions ', () => {
+      expect(myRecipe.getRecipeInstructions()).to.equal(recipeData[0].instructions)
+    });
+  
+    it('Should calculate total recipe cost ', () => {
+      expect(myRecipe.calculateRecipeCost()).to.equal('177.76');
+    });
+});
