67e20dc56c21c233934341f710adedb37e9ac830
Lindsay Schnell <lschnell8@gmail.com>
2021-12-13 14:24:03 -0700

    Fix syntax

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,6 @@
-export const fetchApiData(type) {
-return fetch(`https://what-s-cookin-starter-kit.herokuapp.com/api/v1/${type}`)
-.then((response) => response.json()).catch(error => console.log('fetch error', error))
+export const fetchApiData = (type) => {
+  return fetch(`https://what-s-cookin-starter-kit.herokuapp.com/api/v1/${type}`)
+    .then((response) => 
+    response.json())
+    .catch(error => console.log('fetch error', error))
 };
