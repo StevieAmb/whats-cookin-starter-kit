b0a76aac1f36dd363787981e368d6c9ba86ab20b
Lynn Nguyen <AmandaN22@outlook.com>
2022-01-06 22:28:09 -0700

    Remove console logs and pass test for pantry

--- src/classes/Pantry.js
+++ src/classes/Pantry.js
@@ -20,23 +20,17 @@ class Pantry {
 
   determineIfEnoughIngredientsForRecipe(recipe) {
     let userIngredientAmt = this.showPantryIngredientInfo();
-    console.log(userIngredientAmt);
     for(var i = 0; i < recipe.ingredients.length; i++) {
       let match = false 
       for(var j = 0; j < userIngredientAmt.length; j++) {
         if (recipe.ingredients[i].id === parseInt(userIngredientAmt[j].id)) {
-          console.log('one')
           match = true
         }
         if ((recipe.ingredients[i].id === parseInt(userIngredientAmt[j].id)) && (userIngredientAmt[j].amount < recipe.ingredients[i].quantity.amount)) {
-          console.log('two')
           return "You don't have enough ingredients to cook this recipe";
-         
         }
       }
       if (match === false) {
-        console.log('three')
-        console.log('id', recipe.ingredients[i].id)
        return "You don't have enough ingredients to cook this recipe";
       
       }
@@ -44,6 +38,32 @@ class Pantry {
     return "Let's get cookin', good lookin!"
   }
 }
+
+// determineIfEnoughIngredientsForRecipe(recipe) {
+//   let userIngredientAmt = this.showPantryIngredientInfo(); 
+//   recipe.ingredients.forEach((recipeIngredient) => {
+//     let match = false 
+//     userIngredientAmt.forEach((pantryIngredient) => {
+//       if (recipeIngredient.id === parseInt(pantryIngredient.id)) {
+//         console.log('one')
+//         match = true
+//       }
+//       if ((recipeIngredient.id === parseInt(pantryIngredient.id)) && (pantryIngredient.amount < recipeIngredient.quantity.amount)) {
+//         console.log('two', pantryIngredient.amount)
+//         console.log('twohalf', recipeIngredient.quantity.amount)
+//         return "You don't have enough ingredients to cook this recipe";
+//       }
+//     })
+//     if (match === false) {
+//       console.log('three')
+//       return "You don't have enough ingredients to cook this recipe";
+//   }
+//    })
+//    console.log('four')
+//    return "Let's get cookin', good lookin!"
+//   }
+// }
+
 
 
 export default Pantry;
