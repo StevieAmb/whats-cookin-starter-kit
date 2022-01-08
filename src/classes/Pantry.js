e4da332237b2fb561166965c4ce40aca926df974
Lindsay Schnell <lschnell8@gmail.com>
2022-01-07 17:47:07 -0700

    Rework methods to cover more information needed with less code

--- src/classes/Pantry.js
+++ src/classes/Pantry.js
@@ -1,46 +1,53 @@
+const { ingredientsData } = require("../data/ingredients");
 const { userData } = require("../data/users");
+
 
 class Pantry {
   constructor(user) {
     this.shelf = user.pantry;
   }
 
-  // determineAmountOfIngredientsUserHas(recipe) {
-  //   let userIngredientsAmt = this.shelf.reduce((allUserIngredientAmt, userIngredient) => {
-  //     recipe.ingredients.forEach(recipeIngredient => {
-  //       if (recipeIngredient.id === userIngredient.ingredient)
-  //         allUserIngredientAmt.push(userIngredient.amount)
-  //     })
-  //     return allUserIngredientAmt;
-  //   }, [])
-  //   return userIngredientsAmt
-  // }
-
-  determineAmountOfIngredientsUserHas() {
-    let userIngredientAmt = this.shelf.map(ingredient => {
-      return ingredient.amount 
-    })
-    return userIngredientAmt;
-  }
-  //user ingredients amount is an array of the amount of each ingredient the user
-  //has if it matches the ingredients in the recipe
-
-  determineIfEnoughIngredientsForRecipe(recipe) {
-    let userIngredientAmt = this.determineAmountOfIngredientsUserHas(recipe);
-    let userMessage = "";
-    console.log(userIngredientAmt);
-    userIngredientAmt.forEach(amount => {
-      recipe.ingredients.forEach(ingredient => {
-        if(amount < ingredient.quantity.amount || !userIngredientAmt.includes(ingredient.quantity.amount)) {
-          userMessage = "You don't have enough ingredients to cook this recipe"
-        } else {
-          userMessage = "Let's get cookin', good lookin";
+  showPantryIngredientInfo() {
+    this.shelf.map(userIngredient => {
+      ingredientsData.forEach(ingredient => {
+        if (ingredient.id === userIngredient.ingredient) {
+          userIngredient.name = ingredient.name
         }
       })
     })
-    return userMessage
+    return this.shelf
   }
-}
+  //Could we use the findRecipeIngredientInfo method from the Recipe class to instead of repeating the logic here?
+  determineIfEnoughIngredientsForRecipe(recipe) {
+    let pantryIngredients = this.showPantryIngredientInfo();
+    let recipeIngredients = recipe.findRecipeIngredientInfo();
+    const neededIngredients = recipeIngredients.filter(ingredient => {
+      const enoughIngredients = pantryIngredients.every(item => ingredient.id === item.id && item.amount >= parseInt(ingredient.quantity.amount));
+      if (!enoughIngredients) {
+        return ingredient
+      }
+    })
+    return neededIngredients
+  }
+  
+  
+  findRemainingNeededIngredients(recipe) {
+    let notEnoughorMismatchedIngredients = this.determineIfEnoughIngredientsForRecipe()
+    let userNeededIngredients = notEnoughorMismatchedIngredients.reduce((neededIngredients, ingredient) => {
+      recipe.ingredients.forEach(recipeIngredient => {
+        neededIngredientsObj.push({amountNeeded: recipeIngredient.quantity.amount - ingredient.amount, ingredientName: ingredient.name})
+        return neededIngredients;
+      }, [])
+    })
+      return userNeededIngredients;
+    }
+  }
+  
+  //Create a method that iterates through the needed ingredients returned from determineIfEnoughIngredientsForRecipe
+  //Also iterate through the recipe ingredients and compare the amounts 
+  //Return the amount of each missing ingredient needed
 
+
+};
 
 export default Pantry;
