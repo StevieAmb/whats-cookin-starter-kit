9a4fdd7444dd9ed30585fe9778513df9806be57a
Lindsay Schnell <lschnell8@gmail.com>
2022-01-07 20:31:52 -0700

    Create method to find the amount a user needs to make a recipe

--- src/classes/Pantry.js
+++ src/classes/Pantry.js
@@ -1,5 +1,9 @@
-const { ingredientsData } = require("../data/ingredients");
-const { userData } = require("../data/users");
+const {
+  ingredientsData
+} = require("../data/ingredients");
+const {
+  userData
+} = require("../data/users");
 
 
 class Pantry {
@@ -21,33 +25,48 @@ class Pantry {
   determineIfEnoughIngredientsForRecipe(recipe) {
     let pantryIngredients = this.showPantryIngredientInfo();
     let recipeIngredients = recipe.findRecipeIngredientInfo();
-    const neededIngredients = recipeIngredients.filter(ingredient => {
+    const ingredientsNeeded = recipeIngredients.filter(ingredient => {
       const enoughIngredients = pantryIngredients.every(item => ingredient.id === item.id && item.amount >= parseInt(ingredient.quantity.amount));
       if (!enoughIngredients) {
         return ingredient
+        //returns an array of recipe ingredient objects that are missing from the pantry or less than the amount needed to make the recipe
       }
     })
-    return neededIngredients
+    return ingredientsNeeded
   }
-  
-  
-  findRemainingNeededIngredients(recipe) {
-    let notEnoughorMismatchedIngredients = this.determineIfEnoughIngredientsForRecipe()
-    let userNeededIngredients = notEnoughorMismatchedIngredients.reduce((neededIngredients, ingredient) => {
-      recipe.ingredients.forEach(recipeIngredient => {
-        neededIngredientsObj.push({amountNeeded: recipeIngredient.quantity.amount - ingredient.amount, ingredientName: ingredient.name})
-        return neededIngredients;
-      }, [])
-    })
-      return userNeededIngredients;
-    }
-  }
-  
-  //Create a method that iterates through the needed ingredients returned from determineIfEnoughIngredientsForRecipe
-  //Also iterate through the recipe ingredients and compare the amounts 
-  //Return the amount of each missing ingredient needed
 
+  findAmountNeeded(recipe) {
+    let recipeIngredients = recipe.findRecipeIngredientInfo();
+    const newShelf = recipeIngredients.map(recipeIngredient => {
+      this.shelf.forEach(pantryIngredient => {
+        if (recipeIngredient.id === pantryIngredient.ingredient) {
+          recipeIngredient.amountNeeded = pantryIngredient.amount + (parseInt(recipeIngredient.quantity.amount) - pantryIngredient.amount)
+        }
+        if (recipeIngredient.id !== pantryIngredient.ingredient) {
+          recipeIngredient.amountNeeded = recipeIngredient.quantity.amount
+        }
+      })
+        return recipeIngredient
+    })
+    return newShelf.filter(ingredient => ingredient.amountNeeded)
+  }
 
 };
+
+//
+
+// let userNeededIngredients = notEnoughorMismatchedIngredients.reduce((neededIngredients, ingredient) => {
+//   recipeInfo.ingredients.forEach(recipeIngredient => {
+//     if (recipeIngredient.name === ingredient.name) {
+//       neededIngredients.push({ amountNeeded: recipeIngredient.quantity.amount - ingredient.amount, ingredientName: ingredient.name })
+//     }
+//   })
+//   return neededIngredients;
+// }, [])
+// return userNeededIngredients;
+
+//Create a method that iterates through the needed ingredients returned from determineIfEnoughIngredientsForRecipe
+//Also iterate through the recipe ingredients and compare the amounts 
+//Return the amount of each missing ingredient needed
 
 export default Pantry;
