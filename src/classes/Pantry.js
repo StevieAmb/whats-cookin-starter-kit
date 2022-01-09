679a5d42a73749433b6851df28f7b808e0de81c3
Lindsay Schnell <lschnell8@gmail.com>
2022-01-09 01:19:42 -0700

    Remove nonworking methods

--- src/classes/Pantry.js
+++ src/classes/Pantry.js
@@ -63,106 +63,16 @@ class Pantry {
   //     },
 
   determineIfEnoughIngredientsForRecipe(recipe) {
-    // let thisStupidArray = [];
     let pantryIngredients = this.showPantryIngredientInfo();
-      // console.log('PANTRY', pantryIngredients.map(item => item.name));
     let recipeIngredients = recipe.findRecipeIngredientInfo();
-      // console.log('RECIPE', recipeIngredients.map(ingredient => ingredient), 'PANTRY', pantryIngredients.map(item => `${item.name} - ${item.amount}`));
       return recipeIngredients.reduce((acc, ingredient) => {
       let pantryItemStatus = pantryIngredients.some((item) => (item.name === ingredient.name && item.amount >= parseInt(ingredient.quantity.amount)))
       if (!acc.includes(ingredient) && !pantryItemStatus) {
         acc.push(ingredient)
-        // console.log('INGR', ingredient.name)
-        // console.log('STUPID ARRAY', thisStupidArray.map(elem => elem.name))
-        // console.log('Recipe', ingredient.name)
-        // console.log('Pantry', item.name)
-        // console.log('INGREDIENT', ingredient)
-        // console.log('one')
-        // acc.push(ingredient)
-        // } else if (ingredient.id === item.ingredient && item.amount < parseInt(ingredient.quantity.amount)) {
-          // console.log('two')
-          // acc.push(ingredient)
-          // return ingredient
-    } //else if (ingredient.id === item.ingredient && item.amount < parseInt(ingredient.quantity.amount))
-          // console.log('ITEM: ', pantryIngredients, 'INGR: ', ingredient)
-            // acc.push(ingredient)
-            // return acc; //returns an array of recipe ingredient objects that are missing from the pantry or less than the amount needed to make the recipe
+      } 
       return acc
     }, []);
   }
-  // determineIfEnoughIngredientsForRecipe(recipe) {
-  //   let pantryIngredients = this.showPantryIngredientInfo();
-  //   console.log('PANTRY', pantryIngredients);
-  //   let recipeIngredients = recipe.findRecipeIngredientInfo();
-  //   console.log('RECIPE', recipeIngredients);
-  //   const ingredientsNeeded = pantryIngredients.forEach((item) => {
-  //     const enoughIngredients = recipeIngredients.reduce((acc, ingredient) => {
-  //       // console.log('ITEM', item)
-  //       if (ingredient.id === item.ingredient) {
-  //         console.log('Recipe Ingredient', ingredient.id)
-  //         console.log('Pantry Item', item.ingredient)
-  //         // console.log('INGREDIENT', ingredient)
-  //         console.log('one')
-  //         acc.push(ingredient)
-  //         // return ingredient
-  //       } else if (ingredient.id === item.ingredient && item.amount < parseInt(ingredient.quantity.amount)) {
-  //         console.log('two')
-  //         acc.push(ingredient)
-  //         // return ingredient
-  //       }
-  //       // acc.push(ingredient)
-  //       return acc;//returns an array of recipe ingredient objects that are missing from the pantry or less than the amount needed to make the recipe
-  //     }, [])
-  //     });
-        // if(!acc.includes(enoughIngredients)) {
-          // acc.push(enoughIngredients)
-        // }
-    // console.log('THIS', ingredientsNeeded)
-    // return ingredientsNeeded
-
-  // determineIfEnoughIngredientsForRecipe(recipe) {
-  //   let pantryIngredients = this.showPantryIngredientInfo();
-  //   let recipeIngredients = recipe.findRecipeIngredientInfo();
-  //   const ingredientsNeeded = recipeIngredients.filter(ingredient => {
-  //     if (pantryIngredients.find(item => ingredient.id === item.ingredient && item.amount >= parseInt(ingredient.quantity.amount))) {
-  //     console.log('IDs', ingredient.id === item.ingredient, 'AMOUNTS', item.amount >= parseInt(ingredient.quantity.amount))
-  //     console.log(ingredient)
-  //       return ingredient
-  //     }
-
-  //       //returns an array of recipe ingredient objects that are missing from the pantry or less than the amount needed to make the recipe
-
-  //   })
-  //   return ingredientsNeeded
-  // }
-  // determineIfEnoughIngredientsForRecipe(recipe) {
-  //   let pantryIngredients = this.showPantryIngredientInfo();
-  //   let recipeIngredients = recipe.findRecipeIngredientInfo();
-  //   return pantryIngredients.reduce((acc, item) => {
-  //     const ingredientsNeeded = recipeIngredients.filter((ingredient) => (ingredient.id !== item.ingredient || item.amount >= parseInt(ingredient.quantity.amount)))
-  //     acc = ingredientsNeeded
-  //     return acc  
-  //   }, [])
-  //   // return value
-  // }
-  //Could we use the findRecipeIngredientInfo method from the Recipe class to instead of repeating the logic here?
-  // determineIfEnoughIngredientsForRecipe(recipe) {
-  //   let pantryIngredients = this.showPantryIngredientInfo();
-  //   let recipeIngredients = recipe.findRecipeIngredientInfo();
-  //   const ingredientsNeeded = recipeIngredients.reduce((acc, ingredient) => {
-  //     const enoughIngredients = pantryIngredients.every(item => ingredient.id === item.ingredient && (item.amount >= parseInt(ingredient.quantity.amount)))
-
-  //     if (enoughIngredients) {
-  //       console.log('YUP')
-  //       acc.push(ingredient)
-  //       //returns an array of recipe ingredient objects that are missing from the pantry or less than the amount needed to make the recipe
-  //     }
-  //   }, [])
-  //   return ingredientsNeeded //Should return an array of ingredients that user is missing or doesnt have enough of 
-  // }
-  // // console.log('RecipeID', ingredient.id, 'pantryID', item.ingredient)
-  // // console.log('RecipeAmount', parseInt(ingredient.quantity.amount), 'pantryAmount', item.amount)
-  // // console.log(item.amount >= parseInt(ingredient.quantity.amount))
   findAmountNeeded(recipe) {
     let recipeIngredients = recipe.findRecipeIngredientInfo();
     const newShelf = recipeIngredients.map(recipeIngredient => {
@@ -174,7 +84,7 @@ class Pantry {
           recipeIngredient.amountNeeded = recipeIngredient.quantity.amount
         }
       })
-      return recipeIngredient
+        return recipeIngredient
     })
     return newShelf.filter(ingredient => ingredient.amountNeeded)
   }
@@ -192,17 +102,6 @@ class Pantry {
       })
       return this.shelf
     }, [])
-    // this.shelf.map(pantryIngredient => {
-    //   recipe.ingredients.forEach(recipeIngredient => {
-    //     if (pantryIngredient.id === recipeIngredient.id) {
-    //       pantryIngredient.amount = pantryIngredient.amount - recipeIngredient.quantity.amount
-    //       if (!pantryIngredient.amount) {
-    //         let index = pantryIngredient.indexof()
-    //         pantryIngredient.splice(index, 1)
-    //       }
-    //     }
-    //   })
-    // })
   }
 
   makeShoppingList(ingredientNames) {
