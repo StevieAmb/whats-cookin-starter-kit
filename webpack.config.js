b4421fdbb8ec7e1906b738a8f86ad9bba158e479
Lynn Nguyen <AmandaN22@outlook.com>
2022-01-08 20:17:19 -0700

    Update webpack for scss

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
+       test: /\.s[ac]ss$/i,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
